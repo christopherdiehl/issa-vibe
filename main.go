c1e7fd3a364834da436f8ef239e2a133c6353d9f
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2018-08-25 09:39:22 -0400

    working on getting form values music

--- main.go
+++ main.go
@@ -14,8 +14,26 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
+func handleMusic(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		index := template.Must(template.ParseFiles("layout.html", "genreform.html"))
+		err := index.ExecuteTemplate(w, "base", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	} else {
+		r.ParseForm()
+		fmt.Println(r.Form)
+		for key, value := range r.Form {
+			fmt.Printf("key: %s, values: %s", key, value)
+		}
+		fmt.Fprintln(w, "Music posted successfully")
+	}
+}
+
 func main() {
 	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/music", handleMusic)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
 	fmt.Println("Please navigate to localhost:8080 to view application in browser")
